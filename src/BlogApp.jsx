66795a3144c294595be0cef6d0f5a5423bdc7c10
JuanCM <juancampuzano2356@gmail.com>
2025-05-02 16:44:16 -0500

    feature: Deletes unuseful files - Add new Files for APP

---
+++ src/BlogApp.jsx
@@ -0,0 +1,5 @@
+export const BlogApp = () => {
+  return (
+    <div className="text-3xl font-bold underline" >BlogApp</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BlogApp } from './BlogApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BlogApp />
   </StrictMode>,
 )
