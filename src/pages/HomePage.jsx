ac5d3359bee172f1a392d86b28eede0d930b8252
JuanCM <juancampuzano2356@gmail.com>
2025-05-05 16:43:10 -0500

    feature: Crea y estructura página de ejemplo inicial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
 export const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+    return (
+        <section className="p-4">
+            <h1 className="text-2xl font-bold">Bienvenido a mi blog</h1>
+            {/* Aquí cargarás dinámicamente la lista de posts */}
+        </section>
+    );
+};
