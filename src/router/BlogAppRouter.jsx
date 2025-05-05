f7707572a3a16e936bbbd64aeda8517bd8f1be95
JuanCM <juancampuzano2356@gmail.com>
2025-05-05 16:52:08 -0500

    feature - BlogAppRouter:
    - Agrega el NavBar
    - Modifica el retorno del componente y agrega diseño y links detallados

--- src/router/BlogAppRouter.jsx
+++ src/router/BlogAppRouter.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import { Navbar } from "../components/Navbar";
 
 export const BlogAppRouter = () => {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/posts/:slug" element={<h1>Posts</h1>} />
-            <Route path="/about" element={<h1>About</h1>} />
-            <Route path="*" element={<h1>404</h1>} />
-        </Routes>
+        <>
+            <Navbar />
+            <main className="p-4 max-w-4xl mx-auto">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/posts/:slug" element={<h1>Posts</h1>} />
+                    <Route path="/about" element={<h1>About</h1>} />
+                    <Route path="*" element={<h1>404</h1>} />
+                </Routes>
+            </main>
+        </>
     );
 };
