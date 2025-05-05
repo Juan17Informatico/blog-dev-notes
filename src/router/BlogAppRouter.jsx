f0c8e6d63335adcb780d42cce35c66e1d5806c14
JuanCM <juancampuzano2356@gmail.com>
2025-05-05 16:41:00 -0500

    feature: Agrega las rutas de la aplicación:
    - / -> home
    - /posts/:slug -> Posts
    - /about -> About
    - /* -> 404

--- src/router/BlogAppRouter.jsx
+++ src/router/BlogAppRouter.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
 
 export const BlogAppRouter = () => {
-  return (
-    <div>BlogAppRouter</div>
-  )
-}
+    return (
+        <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/posts/:slug" element={<h1>Posts</h1>} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+    );
+};
