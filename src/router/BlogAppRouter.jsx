6045bcad443e1574e8be2266bbc46e37a54b10ba
JuanCM <juancampuzano2356@gmail.com>
2025-05-05 16:46:10 -0500

    feature - Agrega el componente page 'HomePage' al router

--- src/router/BlogAppRouter.jsx
+++ src/router/BlogAppRouter.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
 
 export const BlogAppRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/posts/:slug" element={<h1>Posts</h1>} />
             <Route path="/about" element={<h1>About</h1>} />
             <Route path="*" element={<h1>404</h1>} />
