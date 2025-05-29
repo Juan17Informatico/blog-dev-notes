b3f2333fbc520f49bf6d4895698da7fdd7b4aff6
Juan17Informatico <juancampuzano2356@gmail.com>
2025-05-28 22:08:01 -0500

    feature: Add foter next to main content

--- src/router/BlogAppRouter.jsx
+++ src/router/BlogAppRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 export const BlogAppRouter = () => {
     return (
@@ -13,6 +14,7 @@ export const BlogAppRouter = () => {
                     <Route path="*" element={<h1 className="max-w-4xl mx-auto">404 - Página no encontrada</h1>} />
                 </Routes>
             </main>
+            <Footer />
         </>
     );
 };
