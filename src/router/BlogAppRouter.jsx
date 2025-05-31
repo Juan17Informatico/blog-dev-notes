0bbf95b78ffd729318995f252e1565ea6f000286
JuanCM <juancampuzano2356@gmail.com>
2025-05-31 10:27:10 -0500

    feature: Import AboutMe section

--- src/router/BlogAppRouter.jsx
+++ src/router/BlogAppRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { Navbar } from "../components/Navbar";
+import { AboutMe } from "../components/AboutMe";
 import { Footer } from "../components/Footer";
 
 export const BlogAppRouter = () => {
@@ -10,7 +11,7 @@ export const BlogAppRouter = () => {
             <main className="p-4">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<h1 className="max-w-4xl mx-auto">About</h1>} />
+                    <Route path="/about" element={<AboutMe />} />
                     <Route path="*" element={<h1 className="max-w-4xl mx-auto">404 - Página no encontrada</h1>} />
                 </Routes>
             </main>
