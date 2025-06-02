99ce1084d31032eae33b8627e7a497127d179192
Juan17Informatico <juancampuzano2356@gmail.com>
2025-06-02 10:20:18 -0500

    feature(Post) Add initial structure

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,12 @@
 export const Post = () => {
   return (
-    <div>Post</div>
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">titulo</h1>
+      <p className="text-sm text-gray-500 mb-6">fecha</p>
+      <div className="prose prose-lg dark:prose-invert">
+        {/* Suponiendo que el contenido viene en HTML seguro */}
+        {/* <div dangerouslySetInnerHTML={{ __html: post.content }} /> */}
+      </div>
+    </div>
   )
 }
