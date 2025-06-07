5165580c845218d12f5e53c65a9b08d95c10474b
Juan17Informatico <juancampuzano2356@gmail.com>
2025-06-07 09:54:31 -0500

    fix dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import mdPlugin from 'vite-plugin-md'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    mdPlugin(),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.md'],
