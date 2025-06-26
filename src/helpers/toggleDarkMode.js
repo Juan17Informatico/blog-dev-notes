d1669f92356008acc3fd6444cd9c3e64bf92f13f
Juan17Informatico <juancampuzano2356@gmail.com>
2025-06-25 20:08:04 -0500

    feature(toggleDarkMode) Add toggle function to switch theme mode

--- src/helpers/toggleDarkMode.js
+++ src/helpers/toggleDarkMode.js
@@ -1,1 +1,11 @@
-export const toggleDarkMode = () => {}
+export const toggleDarkMode = () => {
+    const root = document.documentElement;
+
+    if (root.classList.contains('dark')) {
+        root.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    } else {
+        root.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+};
