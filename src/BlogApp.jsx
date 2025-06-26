0147f9dba647d19934dfb8218c171d3c32d63d8c
Juan17Informatico <juancampuzano2356@gmail.com>
2025-06-25 20:03:28 -0500

    feature: See if the user has the dark mode saved or a preferecense of system

--- src/BlogApp.jsx
+++ src/BlogApp.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import { BlogAppRouter } from "./router/BlogAppRouter";
 
 export const BlogApp = () => {
+    const saved = localStorage.getItem('theme');
+
+    if (saved === 'dark' || (!saved && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    }
+
     return (
         <BrowserRouter basename="/blog-dev-notes">
             <BlogAppRouter />
