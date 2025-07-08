ea202f1b07ba9dc2cf72e83aca33d2f659240f20
Juan17Informatico <juancampuzano2356@gmail.com>
2025-07-07 20:42:59 -0500

    feature(BlogAppRouter): Add darkmode into layout

--- src/router/BlogAppRouter.jsx
+++ src/router/BlogAppRouter.jsx
@@ -7,7 +7,7 @@ import { Post } from "../components/Post";
 
 export const BlogAppRouter = () => {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
             <Navbar />
             <main className="flex-1 pt-16">
                 <Routes>
